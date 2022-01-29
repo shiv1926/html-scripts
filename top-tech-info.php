eb706eef7f605a1b035a85f14362b4626e5b1c43
shivkumar yadav <shiv.yadav26@gmail.com>
2022-01-29 11:17:52 +0530

    html

---
+++ top-tech-info.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <h4>Top 10 Sites for your career: </h4>
+    <ul>
+        <li>Linkedin </li>
+        <li>Indeed </li>
+        <li>Naukri </li>
+        <li>Monster </li>
+        <li>JobBait </li>
+        <li>Careercloud </li>
+        <li>Dice </li>
+        <li>CareerBuilder </li>
+        <li>Jibberjobber </li>
+        <li>Www.Guide4info.com </li>
+    </ul>
+
+    <h4>Top 10 Tech Skills in demand :</h4>
+    <ul>
+        <li>Machine Learning </li>
+        <li>Mobile Development </li>
+        <li>SEO/SEM Marketing </li>
+        <li>Data Visualization </li>
+        <li>Data Engineering </li>
+        <li>UI/UX Design </li>
+        <li>Cyber-security </li>
+        <li>Cloud Computing/AWS </li>
+        <li>Blockchain </li>
+        <li>IOT Top </li>
+    </ul>
+
+    <h4>Top 10 Sites for Free Online Education: </h4>
+    <ul>
+        <li>Coursera </li>
+        <li>edX </li>
+        <li>Khan Academy </li>
+        <li>Udemy </li>
+        <li>iTunesU Free Courses </li>
+        <li>MIT OpenCourseWare </li>
+        <li>Stanford Online </li>
+        <li>Codecademy </li>
+        <li>Open Culture Online Courses </li>
+    </ul>
+
+    <h4>Top 10 Sites to learn Excel for free: </h4>
+    <ul>
+        <li>Microsoft Excel Help Center </li>
+        <li>Excel Exposure </li>
+        <li>Chandoo </li>
+        <li>Excel Central </li>
+        <li>Contextures </li>
+        <li>Excel Hero b. </li>
+        <li>Mr. Excel </li>
+        <li>Improve Your Excel </li>
+        <li>Excel Easy </li>
+        <li>Excel Jet </li>
+    </ul>
+
+    <h4>Top 10 Sites to review your resume for free: </h4>
+    <ul>
+        <li>Zety Resume Builder </li>
+        <li>Resumonk </li>
+        <li>Resume dot com </li>
+        <li>VisualCV </li>
+        <li>Cvmaker </li>
+        <li>ResumUP </li>
+        <li>Resume Genius </li>
+        <li>Resumebuilder </li>
+        <li>Resume Baking </li>
+        <li>Enhancy </li>
+    </ul>
+
+    <h4>Top 5 Sites for Interview Preparation: </h4>
+    <ul>
+        <li>Ambitionbox </li>
+        <li>AceThelnterview </li>
+        <li>Geeksforgeeks </li>
+        <li>Leetcode </li>
+        <li>Www.guide4info.com </li>
+    </ul>
+</body>
+</html>
